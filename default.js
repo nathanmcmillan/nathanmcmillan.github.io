76595f078f7520019cf9ab4300dfc632c7e964b6
nmcmilla <nmcmilla@ucsd.edu>
2017-01-14 19:23:01 -0800

    added 404 page and better css

--- default.js
+++ default.js
@@ -1,62 +1,16 @@
-			var scrollspeed = 0.075;
-			
-            var scrollx = -1;
-			var targetx = -1;
-			
-			function scrollto(id)
-			{
-				var target = document.getElementById(id);
-				
-				scrollx = self.pageXOffset;
-				targetx = target.offsetLeft;
-
-				scroll();
-			}
-
-			function scroll()
-			{
-				if (self.pageXOffset > targetx)
-				{
-					scrollx += Math.floor((targetx -scrollx) * scrollspeed - 0.5);
-					self.scrollTo(scrollx, 0);
-					
-					if (self.pageXOffset < targetx) self.pageXOffset = targetx;
-					else setTimeout('scroll()', 20);
-				}
-				else if (self.pageXOffset < targetx)
-				{
-					scrollx += Math.ceil((targetx - scrollx) * scrollspeed + 0.5);
-					self.scrollTo(scrollx, 0);
-					
-					if (self.pageXOffset > targetx) self.pageXOffset = targetx;
-					else setTimeout('scroll()', 20);
-				}
-				else
-				{
-					// targetx = -1;
-				}
-			}
-
-function load()
+window.onload = function()
 {
-	/*window.onscroll = function()
-	{
-		if (targetx == -1)
-		{
-			window.scrollTo(scrollx, 0);
-		}
-	};*/
-	
-	window.addEventListener('resize', resize, false);
-	resize();
+    window.addEventListener('resize', resize, false);
+    resize();
 }
 
 function resize()
 {
-	var size = document.body.clientWidth < document.body.clientHeight ? document.body.clientWidth : document.body.clientHeight;
-	size *= 0.14;
-	
-	if (size < 40) size = 40;
-	
-	document.body.style.fontSize = "" + size + "%";
+    var size = document.body.clientWidth < document.body.clientHeight ? document.body.clientWidth : document.body.clientHeight;
+    
+    size *= 0.14;
+    
+    if (size < 40) size = 40;
+    
+    document.body.style.fontSize = "" + size + "%";
 }
