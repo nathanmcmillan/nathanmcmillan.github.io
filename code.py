eb24400012e8fc953c2aa367002ad25851f8c3a1
nmm-linux <gameinbucket@github.com>
2020-06-06 16:46:04 -0400

    Working code syntax highlighting

--- code.py
+++ code.py
@@ -1,4 +1,5 @@
 import os
+import os.path
 import re
 import json
 import html
@@ -8,42 +9,64 @@ syntax_directory = 'code/syntax/'
 
 
 class Rule:
-    def __init__(self, pattern, replace):
-        self.pattern = re.compile(pattern)
-        self.replace = replace
+    def __init__(self, info):
+        try:
+            self.pattern = re.compile(info[0])
+        except Exception as e:
+            print(info[0], e)
+        self.capture = info[1]
 
-    def sub(self, line):
-        return self.pattern.sub(self.replace, line)
+    def surround(self, match, line):
+        out = ''
+        groups = len(match.groups()) + 1
+        if groups != len(self.capture):
+            raise Exception('Capture groups were not the same as match groups')
+        for i in range(groups):
+            span = self.capture[i]
+            if span is None:
+                continue
+            group = match.group(i)
+            out += '<' + span + '>' + html.escape(group) + '</' + span + '>'
+            if i > 0 and i < groups-1:
+                between = line[match.end(i):match.start(i+1)]
+                out += html.escape(between)
+        if groups > 1:
+            out += line[match.end(groups-1):match.end(0)]
+        return out
 
 
-def c_syntax(source):
+def syntax(syntax_file, source):
     rules = []
-    with open(syntax_directory + 'c.json', 'r') as f:
+    with open(syntax_file, 'r') as f:
         d = json.load(f)
+        d = d["syntax"]
         for e in d:
-            s = d[e]
-            rules.append(Rule(e, s))
+            rules.append(Rule(e))
     out = []
-    # TODO
-    # Iterate through list of rules until a match is found, them substring line and repeat
-    # Do not end line until no matches are found on the remainder
-
-    # TODO
-    # Escape lines to html before processing
-    # Also transform regex to HTML first
 
     for line in source:
         end = ''
         while True:
+            best = None
             for rule in rules:
-                line = rule.sub(line)
-                match = 2
-                line = line[2:]
-                continue
-            break
-        # line = html.escape(line)
-        end += liine
+                match = rule.pattern.search(line)
+                if match is None:
+                    continue
+                if best is None:
+                    best = (rule, match)
+                else:
+                    dif = match.start(0) - best[1].start(0)
+                    if dif < 0 or (dif == 0 and match.end(0) > best[1].end(0)):
+                        best = (rule, match)
+            if best is None:
+                break
+            rule = best[0]
+            match = best[1]
+            out += html.escape(line[0:match.start(0)]) + rule.surround(match, line)
+            line = line[match.end(0):]
+        end += html.escape(line)
         out.append(end)
+
     return ''.join(out)
 
 
@@ -52,6 +75,8 @@ def highlight(name):
         source = file.readlines()
     base = os.path.basename(name)
     ext = os.path.splitext(base)[1]
-    if ext == '.c':
-        return c_syntax(source)
+    ext = ext[1:]
+    syntax_file = syntax_directory + ext + '.json'
+    if os.path.isfile(syntax_file):
+        return syntax(syntax_file, source)
     return ''.join(source)
