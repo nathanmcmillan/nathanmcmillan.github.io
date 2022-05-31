4199912842421cc6f15b53a48ba1e3e992893081
nmm-win <email>
2022-05-30 23:40:02 -0400

    Fixed dark links. I hope!

--- css-make.sh
+++ css-make.sh
@@ -1,27 +1,11 @@
 #!/bin/bash -eu
 
-css='css'
+rm -rf css/*
 
-rm -rf $css
+cat template/light.min.css template/style.css > css/index.css
+cat template/resume.light.min.css template/resume.css > css/resume.css
 
-mkdir $css
+rm -rf dark/css/*
 
-cp template/dark.min.css $css/dark.css
-cp template/light.min.css $css/light.css
-
-echo "" >> $css/dark.css
-echo "" >> $css/light.css
-
-cat template/style.css >> $css/dark.css
-cat template/style.css >> $css/light.css
-
-cp template/resume.light.min.css $css/resume.css
-cp template/resume.dark.min.css $css/resume_dark.css
-
-echo "" >> $css/resume.css
-echo "" >> $css/resume_dark.css
-
-cat template/resume.style.css >> $css/resume.css
-cat template/resume.style.css >> $css/resume_dark.css
-
-cp template/code.css $css/code.css
+cat template/dark.min.css template/style.css > dark/css/index.css
+cat template/resume.dark.min.css template/resume.css > dark/css/resume.css
